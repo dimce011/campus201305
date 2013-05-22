21ba5c12b1388f2c115b856925fa4145ec7c82af
larsic <larsic@IB-CAMPUS01.infobip.local>
2013-05-22 09:51:31 +0200

    rest api for documents

--- mpayments-help/mpayments-help-repo-rest/src/main/java/org/infobip/mpayments/help/repo/rest/RestHelpRepoService.java
+++ mpayments-help/mpayments-help-repo-rest/src/main/java/org/infobip/mpayments/help/repo/rest/RestHelpRepoService.java
@@ -1,27 +1,24 @@
 package org.infobip.mpayments.help.repo.rest;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import javax.ejb.Stateless;
-import javax.jcr.Binary;
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
 import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
-import javax.jcr.nodetype.NodeType;
 import javax.naming.InitialContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 import org.codehaus.jackson.JsonGenerationException;
@@ -55,60 +52,78 @@ public class RestHelpRepoService implements RestHelpRepo {
 			InitialContext initialContext = new InitialContext();
 			repository = (Repository) initialContext.lookup("java:jcr/local");
 			session = repository.login(new SimpleCredentials("admin", "admin".toCharArray()));
-			
-			
-//			Node root = session.getRootNode();
-//			Node help = root.addNode("help", NodeType.NT_FOLDER);
-//
-//			Node pp = help.addNode("pp", NodeType.NT_FOLDER);
-//			Node cs = help.addNode("cs", NodeType.NT_FOLDER);
-//			Node wd = help.addNode("wd", NodeType.NT_FOLDER);
-//			Node fi = help.addNode("fi", NodeType.NT_FOLDER);
-//			Node ami = help.addNode("ami", NodeType.NT_FOLDER);
-//
-//			Node service = pp.addNode("service", NodeType.NT_FOLDER);
-//			Node tran = pp.addNode("tran", NodeType.NT_FOLDER);
-//			Node sals = pp.addNode("sals", NodeType.NT_FOLDER);
-//			Node resel = pp.addNode("resel", NodeType.NT_FOLDER);
-//
-//			Node one = service.addNode("1", NodeType.NT_FOLDER);
-//			Node two = service.addNode("2", NodeType.NT_FOLDER);
-//			Node three = service.addNode("3", NodeType.NT_FOLDER);
-//			Node seven = service.addNode("4", NodeType.NT_FOLDER);
 
-//			Node en = one.addNode("en", NodeType.NT_FILE);
-//			//Node cro = one.addNode("cro", NodeType.NT_FILE);
-//
-//			Node content = en.addNode("jcr:content", "nt:resource");
-//
-//			File f = new File("template.ftl");
-//			InputStream stream = new BufferedInputStream(new FileInputStream(f));
-//			Binary binary = session.getValueFactory().createBinary(stream);
-//			content.setProperty("jcr:data", binary);
+			Node root = session.getRootNode();
+			// Node help = root.addNode("help", NodeType.NT_FOLDER);
+			//
+			// Node pp = help.addNode("pp", NodeType.NT_FOLDER);
+			// Node cs = help.addNode("cs", NodeType.NT_FOLDER);
+			// Node wd = help.addNode("wd", NodeType.NT_FOLDER);
+			// Node fi = help.addNode("fi", NodeType.NT_FOLDER);
+			// Node ami = help.addNode("ami", NodeType.NT_FOLDER);
+			//
+			// Node service = pp.addNode("service", NodeType.NT_FOLDER);
+			// Node tran = pp.addNode("tran", NodeType.NT_FOLDER);
+			// Node sals = pp.addNode("sals", NodeType.NT_FOLDER);
+			// Node resel = pp.addNode("resel", NodeType.NT_FOLDER);
+			//
+			// Node one = service.addNode("1", NodeType.NT_FOLDER);
+			// Node two = service.addNode("2", NodeType.NT_FOLDER);
+			// Node three = service.addNode("3", NodeType.NT_FOLDER);
+			// Node seven = service.addNode("4", NodeType.NT_FOLDER);
+			// //
+			// Node en = one.addNode("en", NodeType.NT_FILE);
+			// //Node cro = one.addNode("cro", NodeType.NT_FILE);
+			//
+			// Node content = en.addNode("jcr:content", "nt:resource");
 
-//			session.save();
-			ispisiSvuDecu(session.getNode("/help"));
+			// File f = new File("template.ftl");
+			// InputStream stream = new BufferedInputStream(new
+			// FileInputStream(f));
+			// Binary binary = session.getValueFactory().createBinary(stream);
+			// content.setProperty("jcr:data", binary);
+			// session.save();
+
+			// Node root = session.getRootNode();
+			// Node help = session.getNode("/help");
+			//
+			// Node pp = help.addNode("pp", NodeType.NT_FOLDER);
+		
+			/*String path = new java.io.File(".").getCanonicalPath();
+			System.out.println("ovde - "+path);
+			Node one = session.getNode("/help[3]/pp/service/1");
+			Node en = one.addNode("en", NodeType.NT_FILE);
+			Node content = en.addNode("jcr:content", "nt:resource");
+			File f = new File("template.ftl");
 			
+			InputStream stream = new BufferedInputStream(new FileInputStream(f));
+			Binary binary = session.getValueFactory().createBinary(stream);
+			content.setProperty("jcr:data", binary);
+
+			logger.info("name: {}", session.getNode("/help[3]/pp/service/1"));
+			ispisiSvuDecu(session.getNode("/"));
+			session.save();*/
+			//
 
 			// Do something interesting with the Session ...
-//			logger.info("name: {}", sess.getRootNode().getName());
-//			logger.info("type name: {}", sess.getRootNode().getPrimaryNodeType().getName());
-//			logger.info("path: {}", sess.getRootNode().getPath());
-//
-//			Node node = sess.getNode("/");
-//			logger.info("node: {}", node == null ? null : node);
-//
-//			node.setProperty("name", "root");
-//			node.addNode("help", "nt:folder");
-//
-//			node = sess.getNode("/help");
-//			logger.info("node: {}", node == null ? null : node);
-//
-//			node = sess.getNode("/");
-//
-//			logger.info("name: {}", node == null ? null : node.getName());
-//			logger.info("node: {}", node == null ? null : node);
-			
+			// logger.info("name: {}", sess.getRootNode().getName());
+			// logger.info("type name: {}",
+			// sess.getRootNode().getPrimaryNodeType().getName());
+			// logger.info("path: {}", sess.getRootNode().getPath());
+			//
+			// Node node = sess.getNode("/");
+			// logger.info("node: {}", node == null ? null : node);
+			//
+			// node.setProperty("name", "root");
+			// node.addNode("help", "nt:folder");
+			//
+			// node = sess.getNode("/help");
+			// logger.info("node: {}", node == null ? null : node);
+			//
+			// node = sess.getNode("/");
+			//
+			// logger.info("name: {}", node == null ? null : node.getName());
+			// logger.info("node: {}", node == null ? null : node);
 
 		} catch (Exception ex) {
 			error = true;
@@ -147,21 +162,21 @@ public class RestHelpRepoService implements RestHelpRepo {
 
 		return s;
 	}
-	
+
 	private void ispisiSvuDecu(Node node) throws RepositoryException {
 		if (node.hasNodes()) {
 			NodeIterator it = node.getNodes();
 			while (it.hasNext()) {
 				Node dete = it.nextNode();
 				logger.info(dete.getPath());
-				//System.out.println(dete.getPath());
+				// System.out.println(dete.getPath());
 				ispisiSvuDecu(dete);
 			}
 		}
 	}
-	
-	public void createTree(){
-		
+
+	public void createTree() {
+
 	}
 
 	@Override
@@ -173,40 +188,37 @@ public class RestHelpRepoService implements RestHelpRepo {
 		DocumentNode pp = new DocumentNode();
 		DocumentNode cs = new DocumentNode();
 		DocumentNode help = new DocumentNode();
-		
-		
-		
-		
+
 		cs.setCategory("HELP");
 		cs.setKey("1666-sssss-ddddd-33333");
-		
+
 		cs.setSelfPath("/help/cs");
 		cs.setTitle("CS");
 		cs.setType("nt:FOLDER");
-		
+
 		service.setCategory("HELP");
 		service.setKey("1666-ddddd-ddddd-33333");
-		
+
 		service.setSelfPath("/help/cs/service");
 		service.setTitle("service");
 		service.setType("nt:FOLDER");
-		
+
 		tran.setCategory("HELP");
 		tran.setKey("1666-ddddd-99999-33333");
-		
+
 		tran.setSelfPath("/help/cs/tran");
 		tran.setTitle("tran");
 		tran.setType("nt:FOLDER");
-		
+
 		pp.setCategory("HELP");
 		pp.setKey("1111-sssss-ddddd-33333");
-		
+
 		pp.setSelfPath("/help/pp");
 		pp.setTitle("PP");
 		pp.setType("nt:FOLDER");
 		pp.addChild(service);
 		pp.addChild(tran);
-		
+
 		help.setCategory("Help");
 		help.addChild(pp);
 		help.addChild(cs);
@@ -215,16 +227,14 @@ public class RestHelpRepoService implements RestHelpRepo {
 		help.setSelfPath("/help");
 		help.setTitle("Help");
 		help.setType("nt:FOLDER");
-		
+
 		cs.setParent(help);
-		
+
 		pp.setParent(help);
-		
+
 		service.setParent(pp);
 		tran.setParent(pp);
-		
-		
-		
+
 		try {
 			response = jsonMapper.defaultPrettyPrintingWriter().writeValueAsString(pp);
 		} catch (JsonGenerationException e) {
@@ -237,8 +247,11 @@ public class RestHelpRepoService implements RestHelpRepo {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return Response.status(Response.Status.OK).entity(response).build();
 	}
+	
+
+
 
 }
