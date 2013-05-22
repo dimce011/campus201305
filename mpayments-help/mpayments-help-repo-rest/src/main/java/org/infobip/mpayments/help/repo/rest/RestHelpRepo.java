2624c3cb26a2ab6b945c8991f910f7782ed67161
Miroslav Tisma <tisma.etf@gmail.com>
2013-05-22 16:07:14 +0200

    some small changes

--- mpayments-help/mpayments-help-repo-rest/src/main/java/org/infobip/mpayments/help/repo/rest/RestHelpRepo.java
+++ mpayments-help/mpayments-help-repo-rest/src/main/java/org/infobip/mpayments/help/repo/rest/RestHelpRepo.java
@@ -4,7 +4,6 @@ import javax.ejb.Local;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
